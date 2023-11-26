609d8a4ed5ce93cfd7c7a1bdd47e97283f6f6a40
Marcley Colin <marcley.colin@aol.com>
2023-11-26 11:30:58 -0500

    Update TimersAssignment.js

--- src/TimersAssignment.js
+++ src/TimersAssignment.js
@@ -12,13 +12,22 @@ function countDown(num) {
     }
 }
 
+/**
+ * Timer with a randomGame function, Function prints a number
+ * @param none
+ * @returns none
+ */
+let tries = 0;
 let timeIntervalID = setInterval(function randomGame() {
     //pick a number between 0 and 1
     let rand = Math.random() * 1;
     rand *= 100;
     rand = Math.round(rand) / 100;
+    //track tries
+    tries++;
+    //conditional for printing and ending the loop
     if (rand > 0.75) {
-        console.log(rand)
+        console.log(`Random number: ${rand}. Took ${tries} tries.`)
         clearInterval(timeIntervalID);
     }
 }, 1000)
